aafe2b1016387bbf7284b5b07172942fde63f54e
Akuna Akwei <akunaakwei@gmail.com>
2025-10-29 09:07:01 +0100

    add freetype dependency

--- build.zig
+++ build.zig
@@ -5,19 +5,19 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const linkage = b.option(std.builtin.LinkMode, "linkage", "Linkage type for the library") orelse .static;
-    // const hb_have_freetype = b.option(bool, "HB_HAVE_FREETYPE", "Enable freetype interop helpers") orelse false;
+    const hb_have_freetype = b.option(bool, "HB_HAVE_FREETYPE", "Enable freetype interop helpers") orelse false;
     const hb_have_icu = b.option(bool, "HB_HAVE_ICU", "Enable icu unicode functions") orelse false;
 
     const harfbuzz_dep = b.dependency("harfbuzz", .{});
-    // const maybe_freetype_dep = freetype_dep: {
-    //     if (!hb_have_freetype)
-    //         break :freetype_dep null;
+    const maybe_freetype_dep = freetype_dep: {
+        if (!hb_have_freetype)
+            break :freetype_dep null;
 
-    //     break :freetype_dep b.lazyDependency("freetype", .{
-    //         .target = target,
-    //         .optimize = optimize,
-    //     });
-    // };
+        break :freetype_dep b.lazyDependency("freetype2", .{
+            .target = target,
+            .optimize = optimize,
+        });
+    };
     const maybe_icu_dep = icu_dep: {
         if (!hb_have_icu)
             break :icu_dep null;
@@ -37,11 +37,11 @@ pub fn build(b: *std.Build) void {
         }),
         .linkage = linkage,
     });
-    // if (maybe_freetype_dep) |freetype_dep| {
-    //     const freetype = freetype_dep.artifact("freetype");
-    //     harfbuzz.linkLibrary(freetype);
-    //     harfbuzz.root_module.addCMacro("HAVE_FREETYPE", "1");
-    // }
+    if (maybe_freetype_dep) |freetype_dep| {
+        const freetype = freetype_dep.artifact("freetype");
+        harfbuzz.linkLibrary(freetype);
+        harfbuzz.root_module.addCMacro("HAVE_FREETYPE", "1");
+    }
     if (maybe_icu_dep) |icu_dep| {
         const icuuc = icu_dep.artifact("icuuc");
         harfbuzz.linkLibrary(icuuc);
